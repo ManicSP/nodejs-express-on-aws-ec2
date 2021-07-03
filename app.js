3bd017ea197f30e9dffe8364798742b6f47896e6
ManicSP <ramesh.awsmanic@gmail.com>
2021-07-03 19:20:47 +0530

    app.js -error

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.2</p> <p>Ramesh you did the CodeDeploy</p>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.2</p> <p>Ramesh you did the CodeDeploy</p>')
+  ('<p> Node app.js issue is fixed</p>');
 })
 
 app.get('/products', (req, res) => {
